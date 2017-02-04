73db49f66b5f445eb3827ea3424ce471fcf664ce
nasilo <nick.a.asilo@gmail.com>
2017-02-04 16:29:38 -0500

    added test for show page

---
+++ spec/features/dvd/03_user_visits_show_page_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "user visits show page" do
+  scenario "user can get to show page from index" do
+    user_adds_dvd
+    click_link "The Terminator"
+
+    expect(page).to have_content "The Terminator"
+    expect(page).to have_content "Arnold Schwarzenegger"
+    expect(page).to have_content "James Cameron"
+    expect(page).to have_content "James Cameron"
+    expect(page).to have_content "2014-08-26"
+  end
+end
---
+++ spec/support/user_add_dvd_helper.rb
@@ -0,0 +1,9 @@
+module UserAddsDvdHelper
+  def user_adds_dvd(username = "foobington", upc = "027616854735")
+    login_with_facebook(username)
+    visit new_dvd_path
+    fill_in "Upc", with: "027616854735"
+    click_button "Search UPC"
+    click_button "Add DVD"
+  end
+end
