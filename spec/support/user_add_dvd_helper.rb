9b05404a33992d2be474e54f7d13202f69d5f0c7
nasilo <nick.a.asilo@gmail.com>
2017-02-04 16:33:05 -0500

    Fixed small error with UserAddsDvdHelper

--- spec/support/user_add_dvd_helper.rb
+++ spec/support/user_add_dvd_helper.rb
@@ -2,7 +2,7 @@ module UserAddsDvdHelper
   def user_adds_dvd(username = "foobington", upc = "027616854735")
     login_with_facebook(username)
     visit new_dvd_path
-    fill_in "Upc", with: "027616854735"
+    fill_in "Upc", with: upc
     click_button "Search UPC"
     click_button "Add DVD"
   end
