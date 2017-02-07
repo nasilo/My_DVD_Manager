629fa3bc9e2f04eceeacbf366bee19dd8554eb8f
nasilo <nick.a.asilo@gmail.com>
2017-02-07 17:55:26 -0500

    built upcFetch (dont be broken, dont be broken)

--- react/src/components/NewDvdApp.js
+++ react/src/components/NewDvdApp.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
+import upcFetch from './upcFetch';
 
 class NewDvdApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
-      upcCode: '',
+      upcCode: null,
       upcId: null,
-      dvdTitle: '',
-      dvdMpaaRating: '',
-      dvdSynopsis: '',
-      dvdStudio: '',
-      dvdCast: '',
-      dvdWriter: '',
-      dvdProducer: '',
-      dvdDirector: '',
-      dvdReleaseDate: '',
-      dvdRunTime: ''
+      dvdTitle: null,
+      dvdMpaaRating: null,
+      dvdSynopsis: null,
+      dvdStudio: null,
+      dvdCast: null,
+      dvdWriter: null,
+      dvdProducer: null,
+      dvdDirector: null,
+      dvdReleaseDate: null,
+      dvdRunTime: null,
+      dvdPurchasePrice: null,
+      dvdPurchaseLocation: null,
+      dvdUserRating: null
     };
-    
+    this.handleUpcSubmit = this.handleUpcSubmit.bind(this);
   }
 
+  handleUpcSubmit() {
+    upcFetch(this.state.upcCode)
+  }
+
+  handleChangeUpc(event) {
+    this.setState({upcCode: event.target.value});
+  }
+
+  render() {
+    return(
+      <div>
+        <UpcForm
+          onSubmit={this.handleUpcSubmit}
+          onChange={this.handleChangeUpc}
+          upcCode={this.state.upcCode}
+        />
+      </div>
+    )
+  }
 }
 
 export default NewDvdApp;
--- react/src/components/upcFetch.js
+++ react/src/components/upcFetch.js
@@ -1,7 +1,7 @@
 upcFetch(upcCode) {
   let data = {
-      upc: review.id,
-    }
+      upc: upcCode
+    };
     let jsonStringData = JSON.stringify(data);
   fetch(`/api/v1/upcs`, {
     credentials: 'same-origin',
@@ -21,9 +21,29 @@ upcFetch(upcCode) {
   .then(response => response.json())
   .then(body => {
     debugger;
-    let newUsers = body.users;
+    let newUpcId = body.upc.id;
+    let newDvdTitle = body.dvd.title;
+    let newDvdMpaaRating = body.dvd.mpaa_rating;
+    let newDvdSynopsis = body.dvd.synopsis;
+    let newDvdStudio = body.dvd.studio;
+    let newDvdCast = body.dvd.cast;
+    let newDvdWriter = body.dvd.writer;
+    let newDvdProducer = body.dvd.producer;
+    let newDvdDirector = body.dvd.director;
+    let newDvdReleaseDate = body.dvd.release_date;
+    let newDvdRunTime = body.dvd.run_time;
     this.setState({
-      users: newUsers,
+      upcId: newUpcId,
+      dvdTitle: newDvdTitle,
+      dvdMpaaRating: newDvdMpaaRating,
+      dvdSynopsis: newDvdSynopsis,
+      dvdStudio: newDvdStudio,
+      dvdCast: newDvdCast,
+      dvdWriter: newDvdWriter,
+      dvdProducer: newDvdProducer,
+      dvdDirector: newDvdDirector,
+      dvdReleaseDate: newDvdReleaseDate,
+      dvdRunTime: newDvdRunTime,
     });
   .catch(error => console.error(`Error in fetch: ${error.message}`));
 }
