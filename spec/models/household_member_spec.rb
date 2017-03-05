eda1332e338056e3da0edaf65330fa136d8f8a7f
nasilo <nick.a.asilo@gmail.com>
2017-03-05 10:23:09 -0500

    cleaned up some code

--- spec/models/household_member_spec.rb
+++ spec/models/household_member_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe UserHouseholdMember, type: :model do
-  it {should have_valid(:household_member_name).when('Bob', 'Mary-Sue', 'John Doe')}
-  it {should_not have_valid(:household_member_name).when(nil, '')}
+  it { should have_valid(:household_member_name).when('Bob', 'Mary-Sue', 'John Doe') }
+  it { should_not have_valid(:household_member_name).when(nil, '') }
 end
